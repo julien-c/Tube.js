8445c28969a32b9ee58bbfa427ffa43cad4f6e91
Julien Chaumond <chaumond@gmail.com>
2015-05-15 14:50:48 +0200

    Console.log host/port at app launch

--- app.js
+++ app.js
@@ -87,7 +87,11 @@ app.get('/reindex', function(req, res) {
 });
 
 
-var server = app.listen(4444);
+var server = app.listen(4444, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('App listening at http://%s:%s', host, port);
+});
 fs.writeFile('app.lock', process.pid);
 try {
 	fs.mkdirSync('.cache/');
