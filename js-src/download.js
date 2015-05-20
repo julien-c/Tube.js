e926e457593557cc41572edad7ad7b9cd037d17c
Julien Chaumond <chaumond@gmail.com>
2015-05-20 20:44:59 +0200

    Stop redirecting to video page after dl

--- js-src/download.js
+++ js-src/download.js
@@ -1,31 +1,31 @@
 $(function() {
 	var $form = $('form.download');
 	var $input = $form.find('input[type="text"]');
+	var $output = $('pre.output');
 	
 	var source;
 	$form.submit(function(e) {
 		e.preventDefault();
+		$output.empty();
 		if (typeof source !== 'undefined') {
 			source.close();
 		}
 		source = new EventSource('/download/stream?url='+encodeURIComponent($input.val()));
 		source.addEventListener('open', function(e) {
-			$('pre.output').removeClass('hide');
+			$output.removeClass('hide');
 		});
 		source.addEventListener('command', function(e) {
-			$('pre.output').append('<strong>$</strong> '+e.data+'\n');
+			$output.append('<strong>$</strong> '+e.data+'\n');
 		});
 		source.addEventListener('message', function(e) {
-			$('pre.output').append(e.data+'\n');
+			console.log(e.data);
+			$output.append(e.data+'\n');
 		});
 		source.addEventListener('err', function(e) {
-			$('pre.output').append('<span class="err">'+e.data+'</span>'+'\n');
+			$output.append('<span class="err">'+e.data+'</span>'+'\n');
 		});
 		source.addEventListener('redirect', function(e) {
-			$('pre.output').append('\n'+'<span class="redirect">Processing completed. Your video here: <a href="'+e.data+'">'+e.data+'</a></span>'+'\n');
-			setTimeout(function() {
-				window.location = e.data;
-			}, 3000);
+			$output.append('\n'+'<span class="redirect">Processing completed. Your video here: <a href="'+e.data+'">'+e.data+'</a></span>'+'\n');
 		});
 	});
 	
